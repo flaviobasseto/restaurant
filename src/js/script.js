b8e8b0bdf49ed9290c448032fedb8b9dca3ec0d8
Flavio Basseto <flavio_basseto@hotmail.com>
2018-04-05 23:18:40 -0300

    upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,23 @@
-function valida_form() {
-    var x = document.forms["login"]["inputEmail"].value;
-    var atpos = x.indexOf("@");
-    var dotpos = x.lastIndexOf(".");
-    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
-        alert("Não é um endereço de e-mail válido");
-        return false;
-    }
-    return true;
+function mask_cpf(field) {
+    $(field).mask('000.000.000-00', {
+        reverse: true
+    });
+}
+
+function mask_fone(field) {
+    $(field).mask('00 00000-0000', {
+        reverse: true
+    });
+}
+
+function mask_rg(field) {
+    $(field).mask('00.000.000-0', {
+        reverse: true
+    });
+}
+
+function mask_data(field) {
+    $(field).mask('00/00/0000', {
+        reverse: true
+    });
 }
